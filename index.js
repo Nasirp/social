7aa06d9ba5f19b41950545955c6a829334911b76
NasirHussain GitHub <nh246nasir@gmail.com>
2023-09-30 17:23:22 +0530

    tested comiit

--- index.js
+++ index.js
@@ -71,7 +71,7 @@
 // })
 function getRandomInt(min, max) {
     min = Math.ceil(min);
-    max = Math.floor(max);
+    max = Math.floor(max);dfs
     return Math.floor(Math.random() * (max - min)) + min;
   }
   
